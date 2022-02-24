bc90fc7f2b403af553093d83800f5802a2136a48
TDCIAN <tdcian71@gmail.com>
2022-02-24 09:09:34 +0900

    single

---
+++ MakeGitHub/Traits.playground/Contents.swift
@@ -0,0 +1,96 @@
+import RxSwift
+
+let disposeBag = DisposeBag()
+
+enum TraitsError: Error {
+    case single
+    case maybe
+    case completable
+}
+
+print("---- single1 ----")
+Single<String>.just("✅")
+    .subscribe(
+        onSuccess: {
+            print("onSuccess: \($0)")
+        },
+        onFailure: {
+            print("onFailure: \($0)")
+        },
+        onDisposed: {
+            print("disposed")
+        }
+    )
+    .disposed(by: disposeBag)
+
+print("---- single2 ----")
+//Observable<String>.just("✅")
+Observable<String>
+    .create { observer -> Disposable in
+        observer.onError(TraitsError.single)
+        return Disposables.create()
+    }
+    .asSingle()
+    .subscribe(
+        onSuccess: {
+            print("onSuccess: \($0)")
+        },
+        onFailure: {
+            print("onFailure: \($0.localizedDescription)")
+        },
+        onDisposed: {
+            print("disposed")
+        }
+    )
+
+print("---- single 3 ----")
+struct SomeJSON: Decodable {
+    let name: String
+}
+
+enum JSONError: Error {
+    case decodingError
+}
+
+let json1 = """
+    {"name": "park"}
+    """
+
+let json2 = """
+    {"my_name": "young"}
+    """
+
+func decode(json: String) -> Single<SomeJSON> {
+    Single<SomeJSON>.create { observer -> Disposable in
+        guard let data = json.data(using: .utf8),
+              let json = try? JSONDecoder().decode(SomeJSON.self, from: data) else {
+                  observer(.failure(JSONError.decodingError))
+                  return Disposables.create()
+              }
+        
+        observer(.success(json))
+        return Disposables.create()
+    }
+}
+
+decode(json: json1)
+    .subscribe {
+        switch $0 {
+        case .success(let json):
+            print("json1 success: \(json.name)")
+        case .failure(let error):
+            print("json1 error: \(error.localizedDescription)")
+        }
+    }
+    .disposed(by: disposeBag)
+
+decode(json: json2)
+    .subscribe {
+        switch $0 {
+        case .success(let json):
+            print("json2 success: \(json.name)")
+        case .failure(let error):
+            print("json2 error: \(error.localizedDescription)")
+        }
+    }
+    .disposed(by: disposeBag)
