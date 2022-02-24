60de184b83aa4c2963efc048242c0b588715d83f
TDCIAN <tdcian71@gmail.com>
2022-02-24 11:59:31 +0900

    take, takeWhile, enumerated, takeUntil, distinctUntilChanged

--- MakeGitHub/FilteringOperators.playground/Contents.swift
+++ MakeGitHub/FilteringOperators.playground/Contents.swift
@@ -75,3 +75,57 @@ guest.onNext("2")
 
 openTime.onNext("Open!")
 guest.onNext("3")
+
+print("--- take ---") // take와 skip은 서로 반대
+Observable.of("1", "2", "3", "4", "5")
+    .take(3)
+    .subscribe(onNext: {
+        print("onNext: \($0)")
+    })
+    .disposed(by: disposeBag)
+
+print("--- takeWhile ---")
+Observable.of("1", "2", "3", "4", "5")
+    .take(while: {
+        $0 != "3"
+    })
+    .subscribe(onNext: {
+        print("onNext: \($0)")
+    })
+    .disposed(by: disposeBag)
+
+print("--- enumerated ---")
+Observable.of("1", "2", "3", "4", "5", "6")
+    .enumerated()
+    .takeWhile {
+        $0.index < 3
+    }
+    .subscribe(onNext: {
+        print("onNext: \($0)")
+    })
+    .disposed(by: disposeBag)
+
+print("--- takeUntil ---")
+let enroll = PublishSubject<String>()
+let enrollEnded = PublishSubject<String>()
+
+enroll
+    .take(until: enrollEnded) // enrollEnded가 트리거 역할
+    .subscribe(onNext: {
+        print("onNext: \($0)")
+    })
+    .disposed(by: disposeBag)
+
+enroll.onNext("1")
+enroll.onNext("2")
+
+enrollEnded.onNext("End!")
+enroll.onNext("3")
+
+print("--- distinctUntilChanged ---")
+Observable.of("저는", "저는", "앵무새", "앵무새", "앵무새", "앵무새", "입니다", "입니다", "입니다", "저는", "앵무새", "일까요?", "일까요?")
+    .distinctUntilChanged()
+    .subscribe(onNext: {
+        print("onNext: \($0)")
+    })
+    .disposed(by: disposeBag)
