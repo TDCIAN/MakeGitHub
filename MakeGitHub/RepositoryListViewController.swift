157d6579b09f898b56ff46acc8ede5e011a360a0
TDCIAN <tdcian71@gmail.com>
2022-02-26 15:50:12 +0900

    config basic viewcontroller

---
+++ MakeGitHub/RepositoryListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RepositoryListViewController.swift
+//  MakeGitHub
+//
+//  Created by JeongminKim on 2022/02/26.
+//
+
+import UIKit
+
+class RepositoryListViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
+
+
